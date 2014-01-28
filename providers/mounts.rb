dff7850494cc28449c03a2cf3a60ab544fb8926e
Christopher H. Laco <chris.laco@rackspace.com>
2014-01-28 18:05:37 -0500

    Update providers to be rubocop compliant
    - Update rubocop config to include providers/**
    - Update providers to comply with rubocop
    
    Change-Id: Ia28e8b091895cadfe8eac2f722fc19a5218d2519
    Implements: rubocop-for-image-storage

--- providers/mounts.rb
+++ providers/mounts.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 #
 # Cookbook Name:: openstack-object-storage
 # Provider:: mounts
@@ -19,57 +20,58 @@
 # Author: Ron Pedde <ron.pedde@rackspace.com>
 #
 
-require "chef/util/file_edit"
+require 'chef/util/file_edit'
 
 action :ensure_exists do
   proposed_devices = @new_resource.devices
   path = @new_resource.name
   dev_info = {}
 
-  Chef::Log.info("IN MOUNTS")
+  Chef::Log.info('IN MOUNTS')
 
   new_resource.updated_by_last_action(false)
 
   # walk through the devices, gathering information
   proposed_devices.each do |device|
-    next if !::File.exists?("/dev/#{device}")
+    next unless ::File.exists?("/dev/#{device}")
 
     info = {}
-    info["device"] = device
-    info["ip"] = @new_resource.ip
-    info["format"] = @new_resource.format
-    info["uuid"] = Mixlib::ShellOut.new("blkid /dev/#{device} -s UUID -o value").run_command.stdout.strip
-    info["mountpoint"] = info["uuid"].split("-").join("")
-    info["mounted"] = Mixlib::ShellOut.new("mount | grep '#{path}/#{info["mountpoint"]}\'").run_command.status
-    info["size"] = Mixlib::ShellOut.new("sfdisk -s /dev/#{device}").run_command.stdout.to_i / 1024
+    info['device'] = device
+    info['ip'] = @new_resource.ip
+    info['format'] = @new_resource.format
+    info['uuid'] = Mixlib::ShellOut.new("blkid /dev/#{device} -s UUID -o value").run_command.stdout.strip
+    info['mountpoint'] = info['uuid'].split('-').join('')
+    info['mounted'] = Mixlib::ShellOut.new("mount | grep '#{path}/#{info["mountpoint"]}\'").run_command.status
+    info['size'] = Mixlib::ShellOut.new("sfdisk -s /dev/#{device}").run_command.stdout.to_i / 1024
 
-    next if (info["uuid"] == '')
+    next if info['uuid'] == ''
 
-    dev_info[info["uuid"]] = info
+    dev_info[info['uuid']] = info
   end
 
-  Chef::Log.info("Physical Inventory:")
-  dev_info.each do |_,v|
+  Chef::Log.info('Physical Inventory:')
+  dev_info.each do | _, v|
     Chef::Log.info("Device: #{v['device']}, UUID: #{v['uuid']}, Mounted: #{v['mounted']}, Format: #{v['format']}")
   end
 
   # make sure we have a "path"
   Directory(path) do
-    group "swift"
-    owner "swift"
+    group 'swift'
+    owner 'swift'
     recursive true
   end.run_action(:create)
 
   # find what should be mounted, and what IS mounted
-  mounts=node["filesystem"].inject({}) { |hsh, (k,v)| hsh.merge(v["mount"] => k) }
-  valid_mounts = dev_info.inject([]) {|ary, (_,v)| ary << "#{path}/#{v['mountpoint']}"}
-  mountpoints = Dir.new(path).reject {|x| x[/^\./] }.collect { |d| "#{path}/#{d}" }
-  inverted_mounts = dev_info.inject({}) {|hsh,(k,v)| hsh.merge({v["mountpoint"] => v.merge("uuid" => k)})}
-  fstabs=::File.readlines("/etc/fstab").inject({}) do |hash,line|
-    line = line.split("#")[0].split()
+  mounts = node['filesystem'].reduce({}) { |hsh, (k, v)| hsh.merge(v['mount'] => k) }
+  valid_mounts = dev_info.reduce([]) { |ary, (_, v)| ary << "#{path}/#{v['mountpoint']}" }
+  mountpoints = Dir.new(path).reject { |x| x[/^\./] }.map { |d| "#{path}/#{d}" }
+  inverted_mounts = dev_info.reduce({}) { |hsh, (k, v)| hsh.merge(v['mountpoint'] => v.merge('uuid' => k)) }
+  fstabs = ::File.readlines('/etc/fstab').reduce({}) do |hash, line|
+    line = line.split('#')[1].split
     Chef::Log.info("#{line[0]} ... #{line[1]}")
     hash.merge(line[1] => line[0])
-  end.reject { |k,v| !k or !v or !k.length or !v.length }
+  end
+  fstabs.reject! { |k, v| !k || !v || !k.length || !v.length }
 
   Chef::Log.info("Mounts: #{mounts}")
   Chef::Log.info("Valid Mounts: #{valid_mounts}")
@@ -77,16 +79,16 @@ action :ensure_exists do
   Chef::Log.info("Fstabs: #{fstabs}")
 
   # mounts in /srv/node that shouldn't be there
-  (mounts.keys.select{|x| x and x[/^#{path}/]} - valid_mounts).each do |dev|
+  (mounts.keys.select { |x| x && x[/^#{path}/] } - valid_mounts).each do |dev|
     Chef::Log.info("Unmounting #{dev}")
     Mixlib::ShellOut.new("umount #{dev}").run_command if Mixlib::ShellOut.new("mount | grep '#{dev}'").run_command.status
     new_resource.updated_by_last_action(true)
   end
 
   # fstab entries that don't need to be there anymore
-  (fstabs.keys.select {|k| k.start_with? path}  - valid_mounts).each do |dev|
-    fe = Chef::Util::FileEdit.new("/etc/fstab")
-    fe.search_file_delete_line(Regexp.new(dev.gsub("/","\/")))
+  (fstabs.keys.select { |k| k.start_with?(path) } - valid_mounts).each do |dev|
+    fe = Chef::Util::FileEdit.new('/etc/fstab')
+    fe.search_file_delete_line(Regexp.new(dev.gsub('/', '\/')))
     fe.write_file
     new_resource.updated_by_last_action(true)
   end
@@ -104,27 +106,27 @@ action :ensure_exists do
 
   # new, unmounted devices
   (valid_mounts - mounts.keys).each do |mountpoint|
-    info = inverted_mounts[mountpoint.gsub("#{path}/","")]
+    info = inverted_mounts[mountpoint.gsub("#{path}/", '')]
 
     Chef::Log.info("mounting #{mountpoint} (#{info['device']})")
 
     mount_path = "#{path}/#{info['mountpoint']}"
 
     Directory(mount_path) do
-      group "swift"
-      owner "swift"
+      group 'swift'
+      owner 'swift'
       recursive true
     end.run_action(:create)
 
     case info['format']
     when 'ext4'
-      mount_options = "noatime,nodiratime,nobarrier,user_xattr"
+      mount_options = 'noatime,nodiratime,nobarrier,user_xattr'
     when 'xfs'
-      case node["platform"]
-        when "ubuntu","debian"
-          mount_options = "noatime,nodiratime,nobarrier,logbufs=8,nobootwait"
-        else
-          mount_options = "noatime,nodiratime,nobarrier,logbufs=8"
+      case node['platform']
+      when 'ubuntu', 'debian'
+        mount_options = 'noatime,nodiratime,nobarrier,logbufs=8,nobootwait'
+      else
+        mount_options = 'noatime,nodiratime,nobarrier,logbufs=8'
       end
     end
 
@@ -137,7 +139,7 @@ action :ensure_exists do
       action :nothing
     end
 
-    if not fstabs.has_key?(mount_path)
+    unless fstabs.key?(mount_path)
       # then its a brand-new drive, freshly formatted
       Chef::Log.info("Mounting new device #{info['mountpoint']}")
       mt.run_action(:enable)
@@ -147,22 +149,21 @@ action :ensure_exists do
     new_resource.updated_by_last_action(true)
   end
 
-  dev_info.reject { |k,v| v["mounted"] }.keys.each do |uuid|
-    dev_info[uuid]["mounted"] = Mixlib::ShellOut.new("mount | grep '#{path}/#{dev_info[uuid]["mountpoint"]}\'").run_command.status
+  dev_info.reject { |k, v| v['mounted'] }.keys.each do |uuid|
+    dev_info[uuid]['mounted'] = Mixlib::ShellOut.new("mount | grep '#{path}/#{dev_info[uuid]["mountpoint"]}\'").run_command.status
   end
 
-  if @new_resource.publish_attributes and dev_info != {}
-    dev_info.each do |k,v|
-      node.set["swift"]["state"]["devs"][k] = {
-        :device => v["device"],
-        :size => v["size"],
-        :uuid => v["uuid"],
-        :mounted => v["mounted"],
-        :format => v["format"],
-        :mountpoint => v["mountpoint"],
-        :ip => v["ip"]
+  if @new_resource.publish_attributes && dev_info != {}
+    dev_info.each do |k, v|
+      node.set['swift']['state']['devs'][k] = {
+        device: v['device'],
+        size: v['size'],
+        uuid: v['uuid'],
+        mounted: v['mounted'],
+        format: v['format'],
+        mountpoint: v['mountpoint'],
+        ip: v['ip']
       }
     end
   end
 end
-
