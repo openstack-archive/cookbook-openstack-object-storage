1d1c866c6ba89f07bf76d83edf8b4c7569e70413
Mark Vanderwiel <vanderwl@us.ibm.com>
2014-07-29 14:12:54 -0500

    Setting up repo for master Juno Branch
    * Update metadata version
    * Removing Strainer file
    * Adding Gemfile.lock to .gitignore
    * Updating Gems
      * chef (11.12.0)
      * chefspec (4.0.0)
      * rspec (3.0.0)
    
    Change-Id: I8aefcb9bba56f7413f07209c0f658dc96da9f375
    Partial-Bug: 1349865

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'ATT, Inc.'
 license           'Apache 2.0'
 description       'Installs and configures Openstack Swift'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '9.0.3'
+version           '10.0.0'
 
 recipe            'openstack-object-storage::account-server', 'Installs the swift account server'
 recipe            'openstack-object-storage::client', 'Install the switch client'
@@ -19,4 +19,4 @@ end
 depends 'memcached', '>= 1.7.2'
 depends 'statsd', '>= 0.1.5'
 depends 'apt', '>= 2.3.8'
-depends 'openstack-common', '~> 9.0'
+depends 'openstack-common', '~> 10.0'
