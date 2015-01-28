227e879522ebc2e28f59813b1d86e8099d531bb3
Mark Vanderwiel <vanderwl@us.ibm.com>
2015-01-28 16:10:54 -0600

    Use keystone as default authenication
    * Change default from swauth to keystone
    * Add missing authtoken section to swift-proxy.conf
    * Readme cleanups
    * metadata version cleanups
    * another patch will fix up roles in cookbook-chef-repo
    
    Change-Id: Iee5e3d523b584d761848689c6ed55686bbdac313

--- metadata.rb
+++ metadata.rb
@@ -9,15 +9,16 @@ version           '10.0.0'
 recipe            'openstack-object-storage::account-server', 'Installs the swift account server'
 recipe            'openstack-object-storage::client', 'Install the swift client'
 recipe            'openstack-object-storage::container-server', 'Installs the swift container server'
+recipe            'openstack-object-storage::management-server', 'Installs the swift management server'
 recipe            'openstack-object-storage::object-server', 'Installs the swift object server'
 recipe            'openstack-object-storage::proxy-server', 'Installs the swift proxy server'
 recipe            'openstack-object-storage::setup', 'Does initial setup of a swift cluster'
 
-%w{ centos ubuntu }.each do |os|
+%w{ centos ubuntu redhat }.each do |os|
   supports os
 end
 
 depends 'memcached', '>= 1.7.2'
 depends 'statsd', '>= 0.1.5'
-depends 'openstack-common', '~> 10.0'
-depends 'openstack-identity', '~> 10.0'
+depends 'openstack-common', '>= 10.0.0'
+depends 'openstack-identity', '>= 10.0.0'
