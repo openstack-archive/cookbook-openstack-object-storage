79c4a8c57aa0837a5a315ab14d1ad2ad0a6c832f
Mark Vanderwiel <vanderwl@us.ibm.com>
2015-03-05 15:15:53 -0600

    swift updates for roles and services
    * update role to match our chef repo
    * allow platform service names
    * improved logging
    * use memcache list from common
    * remove old unneeded templates and service providers
    * add ring gzone and region attributes
    * cleanup deprecated get_secret calls
    * add templates for sync, expirer and recon
    
    Change-Id: I2373d65b02c56ada1be8703be0492fab3597be26
    Implements: blueprint object-storage-cleanup

--- recipes/identity_registration.rb
+++ recipes/identity_registration.rb
@@ -29,7 +29,7 @@ end
 
 identity_admin_endpoint = admin_endpoint 'identity-admin'
 
-token = get_secret 'openstack_identity_bootstrap_token'
+token = get_password 'token', 'openstack_identity_bootstrap_token'
 auth_url = ::URI.decode identity_admin_endpoint.to_s
 
 admin_api_endpoint = admin_endpoint 'object-storage-api'
